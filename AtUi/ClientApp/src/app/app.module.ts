b3d59007759828deaa6d4a7ec9436b2b08a3f134
aravindugattu <aravindu.gattu@pactera.com>
2019-06-20 18:52:00 +0530

    New Changes

--- AtUi/ClientApp/src/app/app.module.ts
+++ AtUi/ClientApp/src/app/app.module.ts
@@ -33,6 +33,8 @@ import { AdminconfigComponent } from './adminconfig/adminconfig.component';
 import { AlertDialogComponent } from './dialogs/alert-dialog/alert-dialog.component';
 import { DialogService } from './services/dialog.service';
 import { ExcelService } from './services/ExcelExport';
+import { ShippingService } from './services/shipping.service';
+import { HttpService } from './shared/http.service';
 
 import { WorkflowComponent } from './workflow/workflow.component';
 import { WorkflowService } from './services/WorkflowService';
@@ -50,7 +52,6 @@ import { EditableComponent } from './shared/editable/editable.component';
 import { EditModeDirective } from './shared/editable/edit-mode.directive';
 import { ViewModeDirective } from './shared/editable/view-mode.directive';
 import { EditOnEnterDirective } from './shared/editable/edit-on-enter.directive';
-
 
 @NgModule({
   declarations: [
@@ -91,7 +92,7 @@ import { EditOnEnterDirective } from './shared/editable/edit-on-enter.directive'
     AppRoutingModule,
     AgGridModule.withComponents([])
   ],
-  providers: [WorkflowService, EmployeeService,ProjectService, RoleService, UserService, AuthGuard, CustomerService, LoginGuard, AssignProjectService, ExcelService, HomeService, DialogService],
+  providers: [WorkflowService, EmployeeService, ProjectService, RoleService, UserService, AuthGuard, CustomerService, LoginGuard, AssignProjectService, ExcelService, HomeService, DialogService, ShippingService, HttpService],
   bootstrap: [AppComponent],
   entryComponents: [LoginComponent, AlertDialogComponent]
 
--- AtUi/ClientApp/src/app/services/shipping.service.ts
+++ AtUi/ClientApp/src/app/services/shipping.service.ts
@@ -3,18 +3,22 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Http, RequestOptions, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs';
+import { HttpService } from '../shared/http.service';
 
 @Injectable()
 export class ShippingService {
-  constructor(private httpClient: HttpClient, private router: Router) { }
+  constructor(private httpClient: HttpClient, private router: Router, private httpService: HttpService) { }
 
-  getTranslateData(WorkflowID: any) {
+  getTranslate(WorkflowID: any) {
     //const params = new HttpParams()
     //  .set('userId', userId)
     //  .set('password', password);
     //return this.httpClient.get(`api/Login/ValidateUser`, { params })
     //return this.httpClient.get(`https://localhost:44330/api/values/ValidateUser`, { params })
+    this.httpService.makeGetRequest('api/Shipment', [{ wid: 1 }]);
   }
 
-  
+  public getTranslateData(WorkflowID: any): Observable<any[]> {
+    return this.httpService.makeGetRequest('api/Shipment?wid=' + WorkflowID);
+  }
 }
