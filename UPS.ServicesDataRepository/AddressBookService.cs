6de983c75744e63d47ca5facc3d27c9a4fd9590a
Ankita0412 <ankita.yadav@pactera.com>
2019-07-08 18:00:33 +0530

    Implemented bulk address update

---
+++ UPS.ServicesDataRepository/AddressBookService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UPS.ServicesAsyncActions;
+using UPS.ServicesDataRepository.DataContext;
+
+namespace UPS.ServicesDataRepository
+{
+    public class AddressBookService : IAddressBookService
+    {
+        private ApplicationDbContext context;
+        public AddressBookService(ApplicationDbContext applicationDbContext)
+        {
+            this.context = applicationDbContext;
+        }
+        public void InsertAddress()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
