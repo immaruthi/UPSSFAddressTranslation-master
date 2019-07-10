7abae7e9ee269963e0b9d4032314e9c0be6d7ba4
aravindugattu <aravindu.gattu@pactera.com>
2019-07-10 16:41:41 +0530

    Address Book Changes

--- AtUi/ClientApp/src/app/address-book/address-book.component.ts
+++ AtUi/ClientApp/src/app/address-book/address-book.component.ts
@@ -8,6 +8,7 @@ import { DataService } from '../services/data.service';
 import { Observable } from 'rxjs';
 import { NotificationService } from '../services/NotificationService';
 import { AddressBookEditModelComponent } from './address-book-edit-model/address-book-edit-model.component';
+import { AddressBookData } from '../models/AddressBook';
 
 @Component({
   selector: 'app-address-book',
@@ -20,17 +21,14 @@ export class AddressBookComponent implements OnInit {
   //  ['spC_PSL_CD_TE', 'spC_CTY_TE', 'spC_CTR_TE', 'spC_CPY_TE', 'spC_NA', 'spC_SND_PTY_CTC_TE', 'spC_ADR_TE', 'spC_CTC_PH', 'spC_SLIC_NR',];
 
   displayedColumns =
-    ['actions','ShipmentId', 'BatchId', 'Organization', 'StatusCode', 'ConsigneeAddressId', 'ConsigneeAddress',
-      'ConsigneeTranslatedAddress', 'Address_One', 'Address_Two', 'Address_Three', 'Address_Four', 'Road',
-      'City', 'Region', 'Country', 'AddressTypeFlag', 'Longitude', 'Latitude', 'GeoCode', 'GeoCodeError',
-      'BuldingNumber', 'BuildingName', 'Unit', 'Area', 'Bat_Id', 'PostalCode', 'Confidence', 'SemanticCheck',
-      'Accuracy', 'VerifyMatch', 'CreatedDate', 'ModifiedDate'];
+    ['actions', 'shipmentId', 'consigneeAddressId', 'consigneeAddress', 'consigneeTranslatedAddress',
+      'confidence', 'accuracy', 'createdDate', 'modifiedDate', 'organization', 'batchId', 'statusCode', 'address_One', 'address_Two',
+      'address_Three', 'address_Four', 'road', 'city', 'region', 'country', 'addressTypeFlag', 'longitude',
+      'latitude', 'geoCode', 'geoCodeError', 'buldingNumber', 'buildingName', 'unit', 'area', 'bat_Id',
+      'postalCode', 'semanticCheck', 'verifyMatch'];
 
-  private eventsSubscription: any;
-  @Input() events: Observable<void>;
-
-  public ResponseData: any[] = [];
-  dataSource = new MatTableDataSource<Element>();
+  public ResponseData: AddressBookData[] = [];
+  dataSource = new MatTableDataSource<AddressBookData>();
   public errorMessage: string;
   selection = new SelectionModel<any>(true, []);
   filterText: string = '';
@@ -53,19 +51,15 @@ export class AddressBookComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getShipperListData();
+    this.getAddressBookData();
   }
 
 
-  getShipperListData() {
+  getAddressBookData() {
     this.ResponseData = [];
-    this.shipperListService.getShipperListData().subscribe((response: any) => {
+    this.addressBookService.getAddressBookData().subscribe((response: AddressBookData[]) => {
       if (response) {
-        if (response.success === true) {
-          this.ResponseData = response.shipperCompanies;
-        } else {
-          this.ResponseData = [];
-        }
+        this.ResponseData = response;
         this.dataSource.data = this.ResponseData;
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
@@ -82,13 +76,13 @@ export class AddressBookComponent implements OnInit {
     this.dataSource.filter = filterValue;
   }
 
-  startEdit(i: number, addressBookData: any) {
+  startEdit(i: number, addressBookData: AddressBookData) {
     let addressBookDetails = addressBookData;
     const dialogRef = this.dialog.open(AddressBookEditModelComponent, {
       data: {
         Id: addressBookData.id,
-        ConsigneeAddress: '5 /F. NO.3 BLDG. 318  XUEYUAN  ROAD YINZHOU DISTRICT HAISU DISTRICT, ZHEJ', // addressBookData.ConsigneeAddress,
-        ConsigneeTranslatedAddress: '5号 / f。宁波市浙江省菏州市海曙区学院路318栋3楼'// addressBookData.ConsigneeTranslatedAddress
+        ConsigneeAddress: addressBookData.consigneeAddress,
+        ConsigneeTranslatedAddress: addressBookData.consigneeTranslatedAddress
       }
     });
 
@@ -96,24 +90,21 @@ export class AddressBookComponent implements OnInit {
       if (result === 1) {
         let updatedDetails = this.dataService.getDialogData();
 
-        if (updatedDetails.ConsigneeTranslatedAddress == addressBookData.ConsigneeTranslatedAddress) {
+        if (updatedDetails.ConsigneeTranslatedAddress == addressBookData.consigneeTranslatedAddress) {
 
           this.notificationService.openSuccessMessageNotification("No changes found to update");
           return;
         }
 
         const details = {
-          ID: addressBookDetails.id,
-          ConsigneeTranslatedAddress: updatedDetails.ConsigneeTranslatedAddress
+          id: addressBookDetails.id,
+          consigneeTranslatedAddress: updatedDetails.ConsigneeTranslatedAddress
         }
 
         this.addressBookService.updateAddressBook(details).subscribe((response: any) => {
           console.log(response)
 
-          addressBookDetails.shP_ADR_TR_TE = response.shipmentDataRequest.shP_ADR_TR_TE;;
-          addressBookDetails.coD_TE = response.shipmentDataRequest.coD_TE;
-          addressBookDetails.smT_STA_NR = response.shipmentDataRequest.smT_STA_NR;
-          addressBookData.poD_RTN_SVC = response.shipmentDataRequest.poD_RTN_SVC;
+          addressBookDetails.consigneeTranslatedAddress = ''; //response.shipmentDataRequest.shP_ADR_TR_TE;;
           this.notificationService.openSuccessMessageNotification("Data Updated Successfully.");
         },
           error => this.notificationService.openErrorMessageNotification("Error while updating data."))
--- AtUi/ClientApp/src/app/services/AddressBookService.ts
+++ AtUi/ClientApp/src/app/services/AddressBookService.ts
@@ -3,14 +3,15 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpService } from '../shared/http.service';
+import { AddressBookData } from '../models/AddressBook';
 
 @Injectable()
 export class AddressBookService {
   constructor(private httpClient: HttpClient, private router: Router,
     private httpService: HttpService) { }
 
-  public getAddressBookData(): Observable<any> {
-    return this.httpService.makeGetRequest(' ');
+  public getAddressBookData(): Observable<AddressBookData[]> {
+    return this.httpService.makeGetRequest('api/AddressBook/getall');
   }
 
   public updateAddressBook(data: any): Observable<any> {
