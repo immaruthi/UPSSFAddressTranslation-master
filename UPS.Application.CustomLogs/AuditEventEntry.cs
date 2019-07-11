6ecc846b60fbb7e502d900fe1242fe88fa647c29
sravanthi gundavelly <sravanthi.gundavelly@pactera.com>
2019-07-11 13:24:27 +0530

    Added code for Log entry.

--- UPS.Application.CustomLogs/AuditEventEntry.cs
+++ UPS.Application.CustomLogs/AuditEventEntry.cs
@@ -1,18 +1,60 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Diagnostics;
+using System.IO;
+using UPS.DataObjects.LogData;
+using Newtonsoft.Json;
 
 namespace UPS.Application.CustomLogs
 {
     public class AuditEventEntry
     {
-       
+
         public static void WriteEntry(Exception exception)
         {
             using (var myLog = new EventLog())
             {
                 myLog.Source = "Application";   
                 myLog.WriteEntry(exception.Message, EventLogEntryType.Error);
+            }
+        }
+
+
+        public static void LogEntry(LogDataModel logDataModel, string path)
+        {
+
+            LogDataModel logDataModelData = new LogDataModel()
+            {
+                apiTypes = APITypes.QuincusAPI_Translation,
+                dateTime = System.DateTime.Now,
+                LogInformation = new LogInformation()
+                {
+                    LogException = new Exception("Test Message"),
+                    LogRequest = "Test Log Request",
+                    LogResponse = "Test Lod Response"
+                }
+            };
+
+            String JsonData = string.Empty;
+
+            for (int i = 0; i < 2; i++)
+            {
+
+                JsonData += JsonConvert.SerializeObject(logDataModelData) + ",";
+            }
+
+            JsonData = JsonData.Remove(JsonData.Length - 1);
+
+            JsonData = "[" + JsonData + "]";
+
+
+
+            LogDataModel[] logDataModels = JsonConvert.DeserializeObject<LogDataModel[]>(JsonData);
+
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(JsonData + System.Environment.NewLine);
             }
         }
     }
---
+++ UPS.DataObjects/LogData/LogDataModel .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UPS.DataObjects.LogData
+{
+
+    public class LogInformation
+    {
+        public string LogRequest { get; set; }
+        public string LogResponse { get; set; }
+        public Exception LogException { get; set; }
+    }
+
+    public class LogDataModel
+    {
+
+        public string userID { get; set; }
+
+        public DateTime dateTime { get; set; }
+
+        public APITypes apiTypes { get; set; }
+
+        public LogInformation LogInformation { get; set; }
+    }
+
+    public enum APITypes
+    {
+        SFExpress = 1,
+        QuincusAPI_Token = 2,
+        QuincusAPI_Batch = 3,
+        QuincusAPI_Translation = 4,
+        AddressBookSetup = 5,
+        EFCoreContext = 6,
+        Others = 7
+    }
+}
