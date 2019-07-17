7530ec65004c2ebc49243574187ab15ea6602570
Ankita0412 <ankita.yadav@pactera.com>
2019-07-17 16:55:24 +0530

    Added Translation Score Column in address book grid and translation grid

--- AtUi/ClientApp/src/app/shipping-data/translate/translate.component.ts
+++ AtUi/ClientApp/src/app/shipping-data/translate/translate.component.ts
@@ -22,7 +22,7 @@ import { ExcelService } from '../../services/ExcelExport';
 
 export class TranslateComponent implements OnInit {
   displayedColumns =
-    ['select', 'actions', 'wfL_ID', 'smT_STA_NR', 'pkG_NR_TE', 'rcV_CPY_TE', 'rcV_ADR_TE', 'shP_ADR_TR_TE', 'coN_NR', 'acY_TE',
+    ['select', 'actions', 'wfL_ID', 'smT_STA_NR', 'pkG_NR_TE', 'rcV_CPY_TE', 'rcV_ADR_TE', 'shP_ADR_TR_TE', 'translation_score', 'coN_NR', 'acY_TE',
       'dsT_CTY_TE', 'dsT_PSL_TE', 'csG_CTC_TE', 'pH_NR', 'fsT_INV_LN_DES_TE', 'shP_CPY_NA', 'shP_ADR_TE', 'shP_CTC_TE', 'shP_PH_TE',
       'orG_CTY_TE', 'orG_PSL_CD', 'imP_SLC_TE', 'coD_TE', 'poD_RTN_SVC'
     ];
