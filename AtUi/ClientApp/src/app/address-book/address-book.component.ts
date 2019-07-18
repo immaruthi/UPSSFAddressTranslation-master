b76121a47262a5243b010a40ddf8aa9d3de756cb
aravindugattu <aravindu.gattu@pactera.com>
2019-07-18 16:48:53 +0530

    Address Book and Get All

--- AtUi/ClientApp/src/app/address-book/address-book.component.ts
+++ AtUi/ClientApp/src/app/address-book/address-book.component.ts
@@ -21,10 +21,10 @@ export class AddressBookComponent implements OnInit {
   //  ['spC_PSL_CD_TE', 'spC_CTY_TE', 'spC_CTR_TE', 'spC_CPY_TE', 'spC_NA', 'spC_SND_PTY_CTC_TE', 'spC_ADR_TE', 'spC_CTC_PH', 'spC_SLIC_NR',];
 
   displayedColumns =
-    ['actions', 'consigneeAddressId', 'consigneeAddress', 'consigneeTranslatedAddress', 'translation_score',
+    ['actions', 'consigneeCompanyName', 'consigneeAddress', 'consigneeTranslatedAddress', 'translation_score',
       'confidence', 'accuracy', 'createdDate', 'modifiedDate', 'organization', 'batchId', 'statusCode', 'address_One', 'address_Two',
       'address_Three', 'address_Four', 'road', 'city', 'region', 'country', 'addressTypeFlag', 'longitude',
-      'latitude', 'geoCode', 'geoCodeError', 'buldingNumber', 'buildingName', 'unit', 'area', 'bat_Id',
+      'latitude', 'geoCode', 'geoCodeError', 'buldingNumber', 'buildingName', 'unit', 'area',
       'postalCode', 'semanticCheck', 'verifyMatch'];
 
   public ResponseData: AddressBookData[] = [];
